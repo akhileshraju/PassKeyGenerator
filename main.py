940ca54a5f8d90897a3f9e968a3463aea27df467
akhileshraju <rajuah27@gmail.com>
2017-09-20 16:16:25 -0700

    Working commit ! Working on the GUI

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import random
-
+import tkinter
 
 def generate_random_digit():
     digit = random.randint(0, 9)
     return digit
 
 
-def generate_random_passphrase(number_of_digits, passphrase=""):
-    already_exists = True
-    while already_exists:
-        for _ in range(number_of_digits):
-            while True:
-                generated_digit = generate_random_digit()
-                if generated_digit not in passphrase:
-                    passphrase += generated_digit
-                    break
-        already_exists = compare_against_database(passphrase)
+def generate_random_passphrase(passphrase="", number_of_digits=5):
+    for _ in range(number_of_digits):
+        while True:
+            generated_digit = str(generate_random_digit())
+            if generated_digit not in passphrase:
+                passphrase += generated_digit
+                break
+    return passphrase
 
 
 def compare_against_database(passphrase):
@@ -31,11 +29,12 @@ def get_category_from_user():
     print("3. Utilities")
     print("4. Misc Important")
     print("5. Misc Un-important")
+    maximum_number_of_choices = 5
     while True:
         user_response = input("\nEnter your choice : ")
         try:
             user_response = int(user_response)
-            if 0 < user_response <= len(digit_for_category):
+            if 0 < user_response <= maximum_number_of_choices:
                 break
             else:
                 print(f"\nInvalid choice ! Try again")
@@ -44,15 +43,28 @@ def get_category_from_user():
     return user_response
 
 
-def main():
-    digit_for_category = [
-        "FINANCE"         : 1,
-        "MAIL"            : 2,
-        "UTILITIES"       : 3,
-        "MISC IMPORTANT"  : 4,
-        "MISC UNIMPORTANT": 5,
-    ]
+def load_database():
+    database = dict()
+    with open("./existing_passphrase.txt", 'r') as db:
+        passphrase = db.readline().rstrip().lstrip()
+        database[passphrase] = True
+    return database
 
+
+def append_to_database(passphrase):
+    with open("./existing_passphrase.txt", 'a') as db:
+        db.write(f"{passphrase}\n")
+
+
+def main():
+    starting_passphrase = str(get_category_from_user())
+    database = load_database()
+    while True:
+        generated_passphrase = generate_random_passphrase(passphrase=starting_passphrase)
+        if generated_passphrase not in database:
+            break
+    append_to_database(generated_passphrase)
+    print(f"Generated passphrase is - {generated_passphrase}")
 
 
 if __name__ == "__main__":
